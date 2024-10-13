0a897a0d2ba6fb248096299ad0c4bcab077e90be
Franco Santos <engr.franco26@gmail.com>
2024-10-13 19:01:01 +0800

    create signin api route

--- api/controllers/authController.js
+++ api/controllers/authController.js
@@ -1,14 +1,17 @@
 import User from "../models/userModel.js";
 import bcryptjs from 'bcryptjs';
 import { errorHandler } from "../utils/error.js";
+import jwt from "jsonwebtoken";
 
 export const signup = async (req, res, next) => {
     try {
         const { username, email, password } = req.body;
-        console.log("Request body: ", req.body)
+
+        console.log("Sign Up Request body: ", req.body)
+
         // Validations
         if (!username || !email || !password || username === '' || email === '' || password === '') {
-            next(errorHandler(400, 'All fields are required'))
+            return next(errorHandler(400, 'All fields are required'))
         }
 
         // Hash the password
@@ -30,8 +33,47 @@ export const signup = async (req, res, next) => {
         } catch (error) {
             next(error)
         }
-       
     } catch (error) {
         next(error)
     }
 };
+
+export const signin = async (req, res, next) =>{
+    try {
+        const { email, password } = req.body;
+        console.log("Sign In Request body: ", req.body)
+
+        // Validations
+        if (!email || !password || email === '' || password === '') {
+            next(errorHandler(400, 'All fields are required'))
+        }
+
+        // Try to signin the user
+        try {
+
+            const validUser = await User.findOne({email})
+            if(!validUser){
+                return next(errorHandler(404 , 'User not found'))
+            }
+
+            const validPassword = bcryptjs.compareSync(password, validUser.password)
+            if(!validPassword){
+                return next(errorHandler(400, 'Invalid email or password'))
+            }
+
+            const token = jwt.sign({id: validUser._id}, process.env.JWT_SECRET, {expiresIn: '1d'})
+
+            const {password: pass, ...rest} = validUser._doc
+
+            res.status(200).cookie('access_token', token,{
+                httpOnly:true}).json(rest)
+
+
+        } catch (error) {
+            next(error)
+        }
+    } catch (error) {
+        next(error)
+    }
+   
+}
--- api/utils/error.js
+++ api/utils/error.js
@@ -1,5 +1,6 @@
 
 export const errorHandler = (statusCode, message) => {
+    console.log("error message", message)
     const error = new Error()
     error.statusCode = statusCode
     error.message = message 
