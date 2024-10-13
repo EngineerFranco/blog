e58583c77618a49a72f3242e6e565f9665582a9b
Franco Santos <engr.franco26@gmail.com>
2024-10-14 00:46:15 +0800

    update UI with currentUserData

--- client/src/components/Header.jsx
+++ client/src/components/Header.jsx
@@ -1,11 +1,13 @@
 
-import { Button, Navbar, TextInput } from "flowbite-react";
+import { Avatar, Button, Dropdown, Navbar, TextInput } from "flowbite-react";
 import { Link, useLocation } from 'react-router-dom';
 import { CiSearch } from "react-icons/ci";
 import { CiDark } from "react-icons/ci";
+import { useSelector } from "react-redux"
 
 const Header = () => {
   const path = useLocation().pathname
+  const {currentUser} = useSelector(state => state.user)
   return (
     <header>
       <Navbar className='border-b-2' fluid rounded>
@@ -28,11 +30,39 @@ const Header = () => {
           <Button className="w-12 h-10 rounded-full hidden sm:inline " color="gray">
             <CiDark className=""/>
           </Button>
-          <Link to="/sign-in"> 
-          <Button color="dark" className="" outline >
-            Sign In
-          </Button>
-          </Link>
+          {
+            currentUser?
+            (<Dropdown
+              arrowIcon={false}
+              inline
+              label={
+                <Avatar
+                alt="user"
+                img={currentUser.data.profilePicture}
+                rounded/>
+              }
+            >
+              <Dropdown.Header>
+                <span className="block text-sm">@{currentUser.data.username}</span>
+                <span className="block truncate text-sm font-medium">{currentUser.data.email}</span>
+              </Dropdown.Header>
+              <Link to={'/dashboard?tab=profile'}>
+                <Dropdown.Item>Profile</Dropdown.Item>
+              </Link>
+              <Dropdown.Item>Settings</Dropdown.Item>
+              <Dropdown.Divider />
+              <Dropdown.Item>Sign out</Dropdown.Item>
+            </Dropdown>
+            )
+            :
+            (<Link to="/sign-in">
+              <Button color="dark" className="" outline >
+                Sign In
+              </Button>
+            </Link>)
+          }
+         
+          
           <Navbar.Toggle/>
         </div>
         <Navbar.Collapse>
