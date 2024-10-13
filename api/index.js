3a88946bf49cf292284dcc1bdac584f6c0f97d21
Franco Santos <engr.franco26@gmail.com>
2024-10-13 13:57:25 +0800

    Conntect to mongoDB

--- api/index.js
+++ api/index.js
@@ -1,8 +1,18 @@
 import express from 'express'
+import mongoose from 'mongoose'
+import dotenv from 'dotenv'
+dotenv.config()
+
+mongoose.connect(process.env.DB_URI).then(
+    () => {console.log('Database is connected'),
+        app.listen(3000, () =>{
+            console.log('Server is listening to port 3000')
+        })
+    }
+).catch(err =>{
+    console.log(err)
+})
 
 const app = express()
 
 
-app.listen(3000, () =>{
-    console.log('Server is listening to port 3000')
-})
