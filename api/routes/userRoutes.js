96c1519bea835b95097b37bf870507652383d817
Franco Santos <engr.franco26@gmail.com>
2024-10-17 19:12:49 +0800

    complete api/server user get/view

--- api/routes/userRoutes.js
+++ api/routes/userRoutes.js
@@ -1,12 +1,14 @@
 import express from 'express'
-import { deleteUser, signoutUser, test, updateUser } from '../controllers/userController.js';
+import { deleteUser, signoutUser, test, updateUser, viewUsers } from '../controllers/userController.js';
 import { verifyToken } from '../utils/verifyUser.js';
 
 const router = express.Router();
 
 router.get('/test', test)
+
 router.put('/update/:id', verifyToken , updateUser)
 router.delete('/delete/:id', verifyToken, deleteUser)
-router.post('/signout', signoutUser) 
+router.post('/signout', signoutUser)
+router.get('/view', verifyToken, viewUsers)
 
 export default router
