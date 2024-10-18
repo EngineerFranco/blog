a62a38832e539abc95bbc968b37f15d27c6fe0a7
Franco Santos <engr.franco26@gmail.com>
2024-10-19 04:38:02 +0800

    complete comment functionality(like, delete, edit. saved)

--- api/routes/commentRoutes.js
+++ api/routes/commentRoutes.js
@@ -1,11 +1,20 @@
 import express from 'express'
-import { createComment, viewComment } from '../controllers/commentController.js';
+import { createComment, deleteComment, editComment, getcomments, likeComment, viewComment } from '../controllers/commentController.js';
 import { verifyToken } from '../utils/verifyUser.js'
 
 const router = express.Router();
 
+// comment
 router.post('/comment-create', verifyToken, createComment)
 router.get('/comment-view/:postId', viewComment)
+
+// comment likes
+router.put('/likeComment/:commentId', verifyToken, likeComment)
+router.put('/editComment/:commentId', verifyToken, editComment);
+router.delete('/deleteComment/:commentId', verifyToken, deleteComment);
+router.get('/getcomments', verifyToken, getcomments);
+
+
 
 
 
